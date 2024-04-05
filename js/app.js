32a64458a171eb172e77d3b6eb1debabf3c40623
prashantkunvat <160098447+prashantkunvat@users.noreply.github.com>
2024-04-05 16:35:45 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -102,7 +102,7 @@ async function displayAlbums() {
   for (let index = 0; index < array.length; index++) {
     const e = array[index];
 
-    if (e.href.includes("/songs/")) {
+    if (e.href.includes("/songs/") !e.href.includes(".htaccess")) {
       let folder = e.href.split("/").slice(-2)[1];
 
       // get the mata data of the folder
